e204bc6f8ff3966f35bba847d6dc84a4c1ade466
dave8git@gmail.com <dave8git@gmail.com>
2020-10-02 17:40:42 +0200

    add player and computer move variables

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-let computerMove = `kamień`;
+let computerMove = 'nieznany ruch';
 let playerMove = `papier`;
+
+let randomNumber = Math.floor((Math.random()*3)+1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'); 
+
+console.log('Gracz wpisał: ' + playerInput); 
+
+
+if(playerInput == 1){
+    playerMove = 'kamień';
+  } else if (playerInput == 2) {
+      playerMove = 'papier';
+  } else {
+      playerMove = 'nożyce';
+  }
 
 function printMessage(msg){
 	let div = document.createElement('div');

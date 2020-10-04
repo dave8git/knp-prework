6122a8db71453912d37cdd75920486bf63b3235f
dave8git@gmail.com <dave8git@gmail.com>
2020-10-04 20:58:22 +0200

    add functions

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
-let computerMove = 'nieznany ruch';
-let playerMove = `papier`;
-
 let randomNumber = Math.floor((Math.random()*3)+1);
+let computerMove = getMoveName(randomNumber);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'); 
+let playerMove = getMoveName(playerInput);
+
+console.log(computerMove); 
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-    computerMove = 'papier';
-} else {
-    computerMove = 'nożyce';
-}
-
 printMessage('Mój ruch to: ' + computerMove);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'); 
 
 console.log('Gracz wpisał: ' + playerInput); 
 
-
-if(playerInput == 1){
-    playerMove = 'kamień';
-  } else if (playerInput == 2) {
-      playerMove = 'papier';
-  } else {
-      playerMove = 'nożyce';
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    } else if (argMoveId == 2) {
+        return 'papier';
+    } else if (argMoveId == 3) {
+        return 'nożyce';
+    } else {
+      printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+      return 'nieznany ruch';
+    }
   }
 
   printMessage('Twój ruch drogi userze to: ' + playerInput);
 
-  if (isNaN(playerInput)||(playerInput>3)) {
-    printMessage('Tak to sobie graj sam!');
-  } else if ((computerMove == 'kamień' && playerMove == 'papier') || (computerMove == 'nożyce' && playerMove == 'kamień') || (computerMove == 'papier' && playerMove == 'nożyce'  )){
-    printMessage('Ty wygrywasz!');
-  } else if (computerMove == playerMove) {
-    printMessage('No i remis!');
-  } else {
-    printMessage('Komputer jest the best!');
-  }
+displayResult(computerMove, playerMove, playerInput);
+ 
+function displayResult(argComputerMove, argPlayerMove, playerInput) {
+    if (isNaN(playerInput) || (playerInput > 3)) {
+        printMessage('Tak to sobie graj sam!');
+    } else if ((computerMove == 'kamień' && playerMove == 'papier') || (computerMove == 'nożyce' && playerMove == 'kamień') || (computerMove == 'papier' && playerMove == 'nożyce')) {
+        printMessage('Ty wygrywasz!');
+    } else if (computerMove == playerMove) {
+        printMessage('No i remis!');
+    } else {
+        printMessage('Komputer jest the best!');
+    }
+}
 
 function printMessage(msg){
 	let div = document.createElement('div');
